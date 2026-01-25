b7c54ec5b7b6de6695761454a4be9aba8c44b4f4
404Found <found404at404notfound@gmail.com>
2026-01-25 23:48:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -214,4 +214,4 @@ def getses():
 if __name__=='__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, threaded=True, host='0.0.0.0')
+    app.run( threaded=True, host='0.0.0.0')
