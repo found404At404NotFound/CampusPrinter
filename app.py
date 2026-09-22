6f9e9cd72141255189c55c5b3dcefd0bd41eb2cd
404Found <found404at404notfound@gmail.com>
2026-09-22 21:07:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -336,6 +336,12 @@ def invoke():
         print("CLERK TOKEN ERROR:", e)
         return jsonify(message="Invalid session"), 401    
 
+
+if __name__=='__main__':
+   
+    app.run(debug=True, threaded=True, host='0.0.0.0',port=5005) 
+  
+
     
 
 
